bb1dae759fb7edcc14a9090b6f5eef14b25bbbaf
Bsal <bishalm626@gmail.com>
2026-05-04 18:38:53 +0545

    Add stderr and stdout logs for flycheck2 build process
    - Created stderr log file capturing blocking file lock messages and build completion.
    - Created stdout log file with detailed JSON output of the compiler artifact, including package ID, manifest path, target details, and build profile information.

---
+++ control_flow/target/debug/deps/control_flow-aebb44cf67a16d09.d
@@ -0,0 +1,5 @@
+D:\Rust\projects\control_flow\target\debug\deps\control_flow-aebb44cf67a16d09.d: src\main.rs
+
+D:\Rust\projects\control_flow\target\debug\deps\libcontrol_flow-aebb44cf67a16d09.rmeta: src\main.rs
+
+src\main.rs:
