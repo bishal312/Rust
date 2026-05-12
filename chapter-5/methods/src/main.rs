1e1939d7a91d1d05f2959df1468b1d97ee294788
Bsal <bishalm626@gmail.com>
2026-05-12 22:24:58 +0545

    Refactor Rectangle methods: improve comments and add width check method

--- chapter-5/methods/src/main.rs
+++ chapter-5/methods/src/main.rs
@@ -1,6 +1,6 @@
 // Methods are similar to functions: We declare them with the fn keyword and a name,
 // they can have parameters and a return value, and they contain some code that’s run when
-// the method is called from somewhere else.  Unlike functions, methods are defined within 
+// the method is called from somewhere else.  Unlike functions, methods are defined within
 // the context of a struct (or an enum or a trait object
 
 #[derive(Debug)]
@@ -13,7 +13,19 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
 }
+
+// The main reason for using methods instead of functions, in addition to providing method syntax
+// and not having to repeat the type of self in every method’s signature, is for organization.
+
+
+//  -> Operator
+// Rust doesn’t have an equivalent to the -> operator; instead, Rust has a feature called automatic
+// referencing and dereferencing. Calling methods is one of the few places in Rust with this behavior.
 
 fn main() {
     println!("<- Methods ->");
@@ -22,8 +34,19 @@ fn main() {
         height: 90,
     };
 
+    let rect2 = Rectangle {
+        width: 400,
+        height: 70,
+    };
+
+    if rect2.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect2.width);
+    }
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
 }
+
+
