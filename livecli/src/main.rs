392e58faa617fb4734dc222e483de9b2bbf5913e
Bishal Kunwar Magar <bishalm626@gmail.com>
2026-06-28 15:10:04 +0545

    Warmup up: CLI project

---
+++ livecli/src/main.rs
@@ -0,0 +1,28 @@
+// A simple cli project in rust
+// This CLI will get a string  in input
+// and return the string reversed
+
+// import the env module to get the command line arguments
+use std::env;
+
+fn main() {
+    // collect the arguments in a vector
+    let args: Vec<String> = env::args().collect();
+
+    // checck if teh user has entered a string
+    // by checking the number of arg
+    // args[0] is the name of the program
+    if args.len() < 2 {
+        println!("Please enter a string");
+        return;
+    }
+
+    // Store the string in a variable
+    let mut input: String = args[1].clone();
+
+    // reverse the string
+    let reversed = input.chars().rev().collect::<String>();
+
+    // print the reversed string
+    println!("Reversed string: {}", reversed);
+}
