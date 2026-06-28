17f337b1ac9cd624975f4158df791a10d1d63be0
Bishal Kunwar Magar <bishalm626@gmail.com>
2026-06-28 14:30:05 +0545

    test part completions and different way of testing

--- chapter-11/adder/src/lib.rs
+++ chapter-11/adder/src/lib.rs
@@ -30,6 +30,27 @@ mod tests {
 
         assert!(larger.can_hold(&smaller));
     }
+
+    // #[test]
+    // fn greeting_contains_name() {
+    //     let result = greeting("Carol");
+    //     assert!(
+    //         result.contains("Carol"),
+    //         "Greeting did not contain name, value was `{result}`"
+    //     );
+    // }
+
+    // #[test]
+    // #[should_panic]
+    // fn greater_than_100() {
+    //     Guess::new(200);
+    // }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
 }
 
 #[derive(Debug)]
@@ -41,5 +62,31 @@ struct Rectangle {
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 || value > 100 {
+//             panic!("Guess value must be between 1 and 100, got {value}.");
+//         }
+
+//         Guess { value }
+//     }
+// }
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {value}.");
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {value}.");
+        }
+
+        Guess { value }
     }
 }
