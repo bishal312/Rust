05c17355f30d0dc2d107f59186c0c5105ae73d40
Bishal Kunwar Magar <bishalm626@gmail.com>
2026-09-03 12:55:41 +0545

    Refactor async message handling in main function to support multiple futures and graceful shutdown

--- chapter-17/spawn-task/src/main.rs
+++ chapter-17/spawn-task/src/main.rs
@@ -3,7 +3,9 @@ use std::time::Duration;
 fn main() {
     // first_fn();
     // second_fn();
-    third_fn();
+    // third_fn();
+    // fourth_fn();
+    number_of_futures()
 }
 // Note: Because all of this async code runs in an async block in a trpl::block_on call,
 // everything within it can avoid blocking. However, the code outside it will 
@@ -50,32 +52,111 @@ fn main() {
 // For another, this program also never exits! Instead, it waits forever
 // for new messages. You will need to shut it down using ctrl-C.
 
-fn third_fn() {
+// fn third_fn() {
+//     trpl::block_on(
+//         async { 
+//                 let (tx,mut rx) = trpl::channel();
+//     let tx_fut =  async {
+//         let vals = vec![
+//             String::from("hi"),
+//             String::from("from"),
+//             String::from("the"),
+//             String::from("future"),
+//             String::from("good"),
+//         ];
+
+//         for val in vals {
+//             tx.send(val).unwrap();
+//             trpl::sleep(Duration::from_millis(500)).await;
+//         }
+//     };
+
+//     let rx_fut = async {
+//         while let Some(value) = rx.recv().await {
+//             println!("received '{value}'");
+//         }
+//     };
+
+//     trpl::join(tx_fut, rx_fut).await;
+//         }
+//     )
+// }
+// we use trpl::join to connect sender and receiver which let program to send message without a delay
+
+// fn fourth_fn() {
+//     trpl::block_on(
+//         async { 
+//                 let (tx,mut rx) = trpl::channel();
+//     let tx_fut =  async {
+//         let vals = vec![
+//             String::from("hi"),
+//             String::from("from"),
+//             String::from("the"),
+//             String::from("future"),
+//             String::from("good"),
+//         ];
+
+//         for val in vals {
+//             tx.send(val).unwrap();
+//             trpl::sleep(Duration::from_millis(500)).await;
+//         }
+//     };
+
+//     let rx_fut = async {
+//         while let Some(value) = rx.recv().await {
+//             println!("received '{value}'");
+//         }
+//     };
+
+//     trpl::join(tx_fut, rx_fut).await;
+//         }
+//     )
+// }
+// When we run this version of the code, it shuts down gracefully after the
+// last message is sent and received. Next, let’s see what would need to change
+// to send data from more than one future. 
+
+fn number_of_futures() {
     trpl::block_on(
-        async { 
-                let (tx,mut rx) = trpl::channel();
-    let tx_fut =  async {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("future"),
-            String::from("good"),
-        ];
+        async {
+            let (tx, mut rx) = trpl::channel();
 
-        for val in vals {
-            tx.send(val).unwrap();
-            trpl::sleep(Duration::from_millis(500)).await;
-        }
-    };
+            let tx1 = tx.clone();
+            let tx1_fut = async move {
+                let vals = vec![
+                    String::from("hi"),
+                    String::from("from"),
+                    String::from("the"),
+                    String::from("program"),
+                ];
 
-    let rx_fut = async {
-        while let Some(value) = rx.recv().await {
-            println!("received '{value}'");
-        }
-    };
+                for val in vals {
+                    tx1.send(val).unwrap();
+                    trpl::sleep(Duration::from_millis(500)).await;
+                }
+            };
 
-    trpl::join(tx_fut, rx_fut).await;
+            let rx_fut = async {
+                while let Some(value) = rx.recv().await {
+                    println!("received '{value}'");
+                }
+            };
+
+            let tx_fut = async move {
+                let vals = vec![
+                    String::from("more"),
+                    String::from("messages"),
+                    String::from("for"),
+                    String::from("you"),
+                ];
+
+                for val in vals {
+                    tx.send(val).unwrap();
+                    trpl::sleep(Duration::from_millis(1500)).await;
+                }
+            };
+
+            trpl::join!(tx1_fut, tx_fut, rx_fut);
         }
     )
 }
